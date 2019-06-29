2a1c124a2de8aafb75d53496739ded8d76a7c2be
Amir Fahd Hadji Usop <dudeamirfahd@gmail.com>
2019-06-29 10:24:32 +0800

    Implement basic master-detail page UI and add basic Track model and API fetching libraries

---
+++ app/src/main/java/com/codingchallenge/app/models/TrackModel.java
@@ -0,0 +1,64 @@
+package com.codingchallenge.app.models;
+
+import com.squareup.moshi.Json;
+
+public class TrackModel extends BaseModel {
+
+    private String trackId;
+    private String trackName;
+    private float trackPrice;
+    @Json(name = "primaryGenreName")
+    private String trackGenre;
+    @Json(name = "longDescription")
+    private String trackDescription;
+    @Json(name = "artworkUrl100")
+    private String trackArtworkUrl;
+
+    public String getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(String trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public float getTrackPrice() {
+        return trackPrice;
+    }
+
+    public void setTrackPrice(float trackPrice) {
+        this.trackPrice = trackPrice;
+    }
+
+    public String getTrackGenre() {
+        return trackGenre;
+    }
+
+    public void setTrackGenre(String trackGenre) {
+        this.trackGenre = trackGenre;
+    }
+
+    public String getTrackDescription() {
+        return trackDescription;
+    }
+
+    public void setTrackDescription(String trackDescription) {
+        this.trackDescription = trackDescription;
+    }
+
+    public String getTrackArtworkUrl() {
+        return trackArtworkUrl;
+    }
+
+    public void setTrackArtworkUrl(String trackArtworkUrl) {
+        this.trackArtworkUrl = trackArtworkUrl;
+    }
+}
